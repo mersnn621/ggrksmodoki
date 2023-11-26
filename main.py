bb342357446eacd045bd5cfeea61087e75371bca
mersnn621 <mersnn621@gmail.com>
2023-11-26 21:18:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+import uvicorn
 
 
 app = FastAPI()
@@ -10,3 +11,6 @@ async def catch_all(path: str, request: Request):
     new_path = f'https://www.google.com/search?q={path}'
     response = templates.TemplateResponse("hoge.html",status_code=302,context={"request": request})
     return response
+
+if __name__ == "__main__":
+    uvicorn.run(app,port=80)
