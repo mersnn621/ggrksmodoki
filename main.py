efe09ef3fc04b0c2fff0e963618c407db0d192a3
mersnn621 <mersnn621@gmail.com>
2023-11-26 21:06:26 +0900

    og対応

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from fastapi import FastAPI
-from fastapi.responses import RedirectResponse
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/{path:path}")
-async def catch_all(path: str):
+async def catch_all(path: str, request: Request):
     new_path = f'https://www.google.com/search?q={path}'
-    response = RedirectResponse(url=new_path, status_code=302)
+    response = templates.TemplateResponse("hoge.html",status_code=302,context={"request": request})
     return response
